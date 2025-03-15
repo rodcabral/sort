7262d72405e0648f90dc78b7807314b647942551
Rodrigo Cabral <rodcabral.ct@gmail.com>
2025-03-15 04:31:59 -0300

    temporary remove merge sort

--- main.c
+++ main.c
@@ -6,22 +6,8 @@ int main(void) {
     App app;
     setup(&app);
     
-    app.current_algorithm = "Merge Sort";
-    shuffle_arr(&app, 200);
-    load_media(&app);
-    merge_sort(&app, app.lines, 0, app.arr_size);
-    if(app.is_sorted) {
-        render(&app, 0);
-        sleep(1);
-    }
-
-    shuffle_arr(&app, 100);
     bubble_sort(&app);
-
-    shuffle_arr(&app, 200);
     selection_sort(&app);
-
-    shuffle_arr(&app, 100);
     insertion_sort(&app);
 
     clean_sdl(&app);
--- src/sort.c
+++ src/sort.c
@@ -1,3 +1,4 @@
+#include "utils.h"
 #include <visualizer.h>
 #include <sort.h>
 
@@ -8,6 +9,7 @@ void swap(int *x, int *y) {
 }
 
 void bubble_sort(App* app) {
+    shuffle_arr(app, 100);
     app->current_algorithm = "Bubble Sort";
     load_media(app);
     for(int i = 0; i < app->arr_size; ++i) {
@@ -37,6 +39,7 @@ void bubble_sort(App* app) {
 }
 
 void selection_sort(App* app) {
+    shuffle_arr(app, 200);
     app->current_algorithm = "Selection Sort";
     load_media(app);
     for(int i = 0; i < app->arr_size; ++i) {
@@ -70,6 +73,7 @@ void selection_sort(App* app) {
 }
 
 void insertion_sort(App* app) {
+    shuffle_arr(app, 100);
     app->current_algorithm = "Insertion Sort";
     load_media(app);
 
@@ -101,78 +105,4 @@ void insertion_sort(App* app) {
     app->is_sorted = true;
     render(app, 0);
     sleep(1);
-}
-
-void merge(App* app, Line* lines, int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    
-    Line L[n1];
-    Line R[n2];
-
-    for(int i = 0; i < n1; ++i) {
-        L[i].val = lines[l + i].val;
-    }
-
-    for(int j = 0; j < n2; ++j) {
-        R[j].val = lines[m + 1 + j].val;
-    }
-
-    int i = 0;
-    int j = 0;
-    int k = l;
-
-    while(i < n1 && j < n2) {
-        if(L[i].val <= R[j].val) {
-            lines[k].val = L[i].val;
-            i++;
-        } else {
-            lines[k] = R[j];
-            j++;
-        }
-        k++;
-        render(app, k);
-
-        while(app->is_paused) {
-            process_input(app);
-            if(!app->is_running) {
-                clean_sdl(app);
-                exit(1);
-            }
-            SDL_RenderCopyF(app->renderer, app->pause_info_texture, NULL, &app->pause_info_props);
-            SDL_RenderPresent(app->renderer);
-        } 
-
-        SDL_Delay(12);
-    }
-
-    while(i < n1) {
-        lines[k].val = L[i].val;
-        i++;
-        k++;
-    }
-
-    while(j < n2) {
-        lines[k].val = R[j].val;
-        j++;
-        k++;
-    } 
-}
-
-void merge_sort(App* app, Line* lines, int l, int r) {
-    if(l < r) {
-        int m = (l+r)/2;
- 
-        if(l >= app->arr_size/2 && l <= (app->arr_size/2) + 1) {
-            SDL_DestroyTexture(app->title_texture);
-            SDL_DestroyTexture(app->info_texture);
-
-            app->current_algorithm = "Merge Sort";
-            load_media(app);
-        }
-        
-        merge_sort(app, lines, l, m);
-        merge_sort(app, lines, m+1, r);
-        merge(app, lines, l, m, r);
-    }
 }
