c99d8233d335773df82f1124f86f15719bdabb58
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2025-07-10 22:44:04 -0300

    setup sdl3

--- include/visualizer.h
+++ include/visualizer.h
@@ -4,8 +4,18 @@
 #define WINDOW_WIDTH 900.0f
 #define WINDOW_HEIGHT 650.0f
 
-typedef struct { } App;
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL3/SDL.h>
+
+typedef struct {
+    bool is_running;
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+} App;
 
 void setup(App *app);
 
 #endif
+
--- src/visualizer.c
+++ src/visualizer.c
@@ -1,3 +1,11 @@
 #include "../include/visualizer.h"
 
-void setup(App* app) { }
+void setup(App* app) {
+    if(!SDL_Init(SDL_INIT_VIDEO)) {
+        fprintf(stderr, "%s\n", SDL_GetError());
+    }
+
+    SDL_CreateWindowAndRenderer("Sort", (int)WINDOW_WIDTH, (int)WINDOW_HEIGHT, SDL_WINDOW_BORDERLESS, &app->window, &app->renderer);
+
+    app->is_running = true;
+}
