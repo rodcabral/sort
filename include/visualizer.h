e6bda2929f11bc5b432e12d8bf58e322987a6cfb
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2025-07-12 16:47:20 -0300

    create line struct

--- include/visualizer.h
+++ include/visualizer.h
@@ -4,12 +4,22 @@
 #define WINDOW_WIDTH 900.0f
 #define WINDOW_HEIGHT 650.0f
 
+#define MAX_LENGTH 250
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL3/SDL.h>
 
 typedef struct {
+    SDL_FRect rect;
+    int value;
+} Line;
+
+typedef struct {
     bool is_running;
+
+    int length;
+    Line lines[MAX_LENGTH];
 
     SDL_Window* window;
     SDL_Renderer* renderer;
