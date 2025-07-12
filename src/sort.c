dce6dfe48bbaf97faf9df0101b4520bd4266493d
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2025-07-12 17:04:21 -0300

    function to shuffle array

--- src/sort.c
+++ src/sort.c
@@ -5,3 +5,11 @@ void swap(int *x, int *y) {
     *x = *y;
     *y = aux;
 }
+
+void shuffle(App* app, int n) {
+    app->length = n;
+    
+    for(int i = 0; i < app->length; ++i) {
+        app->lines[i].value = (int)rand() % (int)app->container.h;
+    }
+}
